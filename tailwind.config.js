e3a5ceefa85e2ef9aaed052beac8647b94ac08d0
vishal kumar <vishakumar041200@gmail.com>
2024-09-06 21:30:17 +0530

    Home Page created

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['src/*'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'primary': ['Klee One', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
+
